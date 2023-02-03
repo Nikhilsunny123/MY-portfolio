10fc73a5a1cefd1e69d3f364731e7f36a4d04a75
Nikhilsunny123 <nikhilsunny35@gmail.com>
2023-02-03 16:33:45 +0530

    fixed resposive

--- components/Layout/Dropdown.js
+++ components/Layout/Dropdown.js
@@ -1,45 +1,80 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
+import menuicon from "../../images/icons/menu-bar.png";
 
 function Dropdown() {
   const [showDropdown, setShowDropdown] = useState(false);
 
   return (
-    <div>
-      <div>
-        <button
-          onClick={() => setShowDropdown(!showDropdown)}
-          className=" hidden md:inline-flex text-white  bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
-        >
-          Dropdown button
-        </button>
-        {showDropdown && (
-          <ul className="absolute bg-white shadow-lg rounded-lg mt-2">
-            <Link href="/">
-              <li className="text-lg cursor-pointer hover:text-green-600">
-                Home
-              </li>
-            </Link>
-            <Link href="/projects">
-              <li className="cursor-pointer hover:text-green-300">Projects </li>
-            </Link>
-            <Link href="/contact">
-              <li className="cursor-pointer hover:text-green-600">Contact </li>
-            </Link>
+    <div className=" group-hover:block  md:hidden sm:hidden ">
+      <Image
+        src={menuicon}
+        onClick={() => setShowDropdown(!showDropdown)}
+        className="text-white  bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
+        width={50}
+        height={50}
+        alt="menu"
+      />
 
-            <li className="cursor-pointer hover:text-green-600">
-              <a
-                href="https://drive.google.com/file/d/1bkMWE792mU17BJGvhkvt3bi0S7qvNsu8/view?usp=share_link"
-                alt="alt text"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Resume
-              </a>{" "}
-            </li>
-          </ul>
-        )}
-      </div>
+      {showDropdown && (
+        <div
+          class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-slate-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabindex="-1"
+        >
+          <div class="py-1" role="none">
+            <ul className="">
+              <Link href="/">
+                <li
+                  className="cursor-pointer hover:text-green-600 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-0"
+                  onClick={() => setShowDropdown(!showDropdown)}
+                >
+                  Home
+                </li>
+              </Link>
+              <Link href="/projects">
+                <li
+                  className="cursor-pointer hover:text-green-600 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-0"
+                  onClick={() => setShowDropdown(!showDropdown)}
+                >
+                  Projects
+                </li>
+              </Link>
+              <Link href="/contact">
+                <li
+                  className="cursor-pointer hover:text-green-600 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-0"
+                  onClick={() => setShowDropdown(!showDropdown)}
+                >
+                  Contact
+                </li>
+              </Link>
+              <li className="cursor-pointer hover:text-green-600 block px-4 py-2 text-sm">
+                <a
+                  href="https://drive.google.com/file/d/1bkMWE792mU17BJGvhkvt3bi0S7qvNsu8/view?usp=share_link"
+                  alt="alt text"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={() => setShowDropdown(!showDropdown)}
+                >
+                  Resume
+                </a>{" "}
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- components/Skills.js
+++ components/Skills.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { skillsdata } from "./projectData";
-import { DisplayData } from "./showData";
 
 function Skills() {
   console.log(...skillsdata);
@@ -9,16 +8,16 @@ function Skills() {
       {/* <DisplayData/> */}
       <h1 className=" text-4xl text-center m-5">I’m Familier with</h1>
       <div className="container flex justify-center w-3/4 md:w-2/3 lg:w-1/2">
-        <div className="flex gap-4 md:grid-flow-row sm:grid-cols-1">
+        <div className="flex flex-col gap-4 sm:grid sm:grid-cols-2 md:grid md:grid-cols-4 xs:grid-cols-2">
           {skillsdata.map((skill) => (
             <div
               className="hover:scale-125 transition-all duration-500 cursor-pointer"
               key={skill.id}
             >
-              <div className="bg-sky-700 rounded-md flex grid-cols-4 items-center flex-col p-2">
+              <div className="bg-sky-700 rounded-md flex items-center flex-col p-2">
                 <Image alt="react" src={skill.image} height={40} width={40} />
+                <p className="p-3 text-m">{skill.name}</p>
               </div>
-              <p className="p-3 text-m">{skill.name}</p>
             </div>
           ))}
         </div>
